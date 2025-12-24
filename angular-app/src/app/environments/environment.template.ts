c8582f63bf74eba1bd7097d709f0605acb1ac7e7
Charlie Davis Cordova Arias <charlie.cordova34@gmail.com>
2025-12-23 22:20:46 -0500

    Agregar template de environment y actualizar gitignore

---
+++ angular-app/src/app/environments/environment.template.ts
@@ -0,0 +1,7 @@
+// environment.template.ts
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tplqzypttzomdczdlety.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbHF6eXB0dHpvbWRjemRsZXR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4MDYwMzYsImV4cCI6MjA3MzM4MjAzNn0.Rpd6Yb8U4CeIAT1ilMbFkkn-ppeHU323balSvqcTXSw',
+    apiUrl: 'http://localhost:3000/api'
+};
