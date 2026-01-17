b2d3d99ae9a8b46ccc0cfd8874430d42f2a80c27
Charlie Davis Cordova Arias <charlie.cordova34@gmail.com>
2026-01-17 00:06:07 -0500

    Agregar template seguro con placeholders (historial 100% limpio)

---
+++ angular-app/src/app/environments/environment.template.ts
@@ -0,0 +1,7 @@
+// environment.template.ts
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://YOUR_PROJECT_ID.supabase.co',
+  supabaseKey: 'YOUR_SUPABASE_ANON_KEY_HERE',
+  apiUrl: 'http://localhost:3000/api'
+};
