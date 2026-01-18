c38c1672c03adae6e954400233643693b4400073
Charlie Davis Cordova Arias <charlie.cordova34@gmail.com>
2026-01-17 21:52:48 -0500

    Mejoras en el gitignore, con el fin de que se noten los environments

---
+++ angular-app/src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+// src/app/environments/environment.ts
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tplqzypttzomdczdlety.supabase.co',  // Tu URL de Supabase
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbHF6eXB0dHpvbWRjemRsZXR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4MDYwMzYsImV4cCI6MjA3MzM4MjAzNn0.Rpd6Yb8U4CeIAT1ilMbFkkn-ppeHU323balSvqcTXSw',  // La nueva anon key que rotaste
+    apiUrl: 'http://localhost:3000'
+};
